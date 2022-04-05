f76f5cafd48e49567d7fdc3c2ec214cf318b2eaa
Daniel Briceño <daniel.b100726@gmail.com>
2022-04-05 13:43:39 -0400

    bug fixed. Add a https API

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   function onSearch(ciudad: string) {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
     )
       .then((r) => r.json())
       .then((recurso) => {
